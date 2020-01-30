612289ab80c807cc21c3e7de34bb383bccedb92d
jack gaarkeuken <jack.gaarkeuken@gmail.com>
2020-01-30 16:13:48 -0500

    update pckg managers

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,43 +1,44 @@
-import React, { Component } from 'react';
-import giphy from 'giphy-api';
+import React, { Component } from "react";
+import giphy from "giphy-api";
 
-import SearchBar from './search_bar.jsx';
-import Gif from './gif.jsx';
-import GifList from './gif_list.jsx';
+import SearchBar from "./search_bar.jsx";
+import Gif from "./gif.jsx";
+import GifList from "./gif_list.jsx";
 
+const GIPHY_API_KEY = "1JgyvGZuR1ICs7kC1HZLR34uIz15EnbL";
 
-const GIPHY_API_KEY = '1JgyvGZuR1ICs7kC1HZLR34uIz15EnbL';
-
-class App extends Component  {
+class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
       gifs: [],
       selectedGifId: null // "3og0IPacuEoA6BBTUI"
-    }
+    };
 
-    this.search('disney');
+    this.search("disney");
   }
 
-  search = (query) => {
-    giphy({ apiKey: GIPHY_API_KEY, https: true })
-      .search({
+  search = query => {
+    giphy({ apiKey: GIPHY_API_KEY, https: true }).search(
+      {
         q: query,
-        rating: 'g',
+        rating: "g",
         limit: 10
-      }, (error, result) => {
+      },
+      (error, result) => {
         this.setState({
           gifs: result.data
         });
-      });
-  }
+      }
+    );
+  };
 
-  selectGif = (id) => {
+  selectGif = id => {
     this.setState({
       selectedGifId: id
-    })
-  }
+    });
+  };
 
   render() {
     return (
